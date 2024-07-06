66f73a67b173f299771e256075eed6b34861fea2
Miguel Zapata <miguelzj0909@gmail.com>
2024-07-06 14:06:44 -0500

    Avance Registro

--- estrapymes/src/app/app.routes.ts
+++ estrapymes/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './view/register/register.component';
+import { HomepageComponent } from './view/homepage/homepage.component';
+export const routes: Routes = [
 
-export const routes: Routes = [];
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+      {
+        path: 'homepage',
+        component: HomepageComponent
+      },
+      {
+        path: '',
+        redirectTo: 'homepage',
+        pathMatch: 'full'
+      }
+];      
+
