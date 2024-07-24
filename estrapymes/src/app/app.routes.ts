39f115e90f6a1cbbcb00f46e692145aa517719b1
johanamileny <johanap687@gmail.com>
2024-07-24 09:20:14 -0500

    intalang2-charts mediante la CLI de Angular para los graficos

--- estrapymes/src/app/app.routes.ts
+++ estrapymes/src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { RegisterComponent } from './views/register/register.component';
 import { HomepageComponent } from './views/homepage/homepage.component';
 import { LoginComponent } from './views/login/login.component';
 import { AuthGuard} from './shared/auth.guard';
+import { AdmindashboardComponent } from './views/admindashboard/admindashboard.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'homepage', component: HomepageComponent},
+  { path: 'admindashboard', component:AdmindashboardComponent },
   { path: '', redirectTo: '/homepage', pathMatch: 'full' }
 ];
 
