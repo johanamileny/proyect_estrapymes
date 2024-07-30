a3c00deff0d55bf256e136d2dc24be56c5ceb34e
johanamileny <johanap687@gmail.com>
2024-07-29 19:13:41 -0500

    crea bd .son y crea targetas en componente graficbowman

--- estrapymes/src/app/componentes/grafic-bowman/grafic-bowman.component.ts
+++ estrapymes/src/app/componentes/grafic-bowman/grafic-bowman.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
 
 @Component({
   selector: 'app-grafic-bowman',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // Incluye CommonModule aquí
   templateUrl: './grafic-bowman.component.html',
-  styleUrl: './grafic-bowman.component.css'
+  styleUrls: ['./grafic-bowman.component.css'] // Corrige 'styleUrl' a 'styleUrls'
 })
-export class GraficBowmanComponent {
+export class GraficBowmanComponent implements OnInit {
+  public quadrants = [
+    { name: 'Sin filigranas', description: 'Características: Probablemente sean especificas al segmento.' },
+    { name: 'Precios bajos', description: 'Riesgo de guerra de precios y margenes reducidos . es necesario ser lider en costos'},
+    { name: 'Hibridas', description: 'Costos reducidos y reinversion en bajos precios y diferenciacion.'},
+    { name: 'Diferenciación' , description: '(A)Sin prima de precio,Valor añadido percibido por el usuario que permite obtener ventajas en cuota de mercado.(B)Diferenciación con prima de precio,valorañadido percibido suficiente para respaldar la prima.' },
+    { name: 'Diferenciación segmentada', description: ' valor añadido en percibido percibido en un segmento en concreto que permite una primaen el precio.' },
+    { name: 'Mayor precio/valor estándar', description: 'Mayores márgenes si los competidores no son seguidores;riesgo de perder cuota de mercado.' },
+    { name: 'Mayor precio/valor reducido', description: 'Solo es factible en una situación de monopolio.' },
+    { name: 'Valor reducido /precio estándar', description: 'Perdida de cuota de mercado.' },
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
+
+
+
--- estrapymes/src/app/views/admindashboard/admindashboard.component.ts
+++ estrapymes/src/app/views/admindashboard/admindashboard.component.ts
@@ -3,15 +3,36 @@ import { BaseChartDirective } from 'ng2-charts';
 import { GraficaradarComponent } from '../../componentes/graficaradar/graficaradar.component';
 import { GraficarelojestraComponent } from '../../componentes/graficarelojestra/graficarelojestra.component';
 import { GraficacirculodoradoComponent } from '../../componentes/graficacirculodorado/graficacirculodorado.component';
+import { UsersService } from '../../services/user.service';
+import { UserListComponent } from '../../componentes/user-list/user-list.component';
+import { User, UserDetailComponent } from '../../componentes/user-detail/user-detail.component';
+import { GraficBowmanComponent } from '../../componentes/grafic-bowman/grafic-bowman.component';
 
 
 @Component({
   selector: 'app-admindashboard',
   standalone: true,
-  imports: [BaseChartDirective,GraficaradarComponent,GraficarelojestraComponent,GraficacirculodoradoComponent],
+  imports: [BaseChartDirective,GraficaradarComponent,GraficarelojestraComponent,
+            GraficacirculodoradoComponent,UserListComponent,UserDetailComponent,GraficBowmanComponent],
   templateUrl: './admindashboard.component.html',
   styleUrl: './admindashboard.component.scss'
 })
-export class AdmindashboardComponent {
 
+export class  AdmindashboardComponent {
+  selectedUser!: User;
+  users: User[] = [];
+  constructor(private usersService: UsersService) {}
+  // Método para actualizar el usuario en la lista
+  updateUser(updatedUser: User) {
+    const index = this.users.findIndex(user => user.id === updatedUser.id);
+    if (index !== -1) {
+      this.users[index] = updatedUser;
+      this.refreshUserList()// Opcional: Puedes emitir un evento para notificar a otros componentes
+    }
+  }
+  refreshUserList() {
+    this.usersService.getUsers().subscribe(users => {
+      this.users = users;
+    });
+  }
 }
