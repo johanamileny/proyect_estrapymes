72c625997eb3527533d9f9d1838d90b13ac79c91
Natalia Quintero Porras <nataliaquinpo@gmail.com>
2024-07-30 22:07:30 -0500

    Fix: ajustando las fuentes

--- estrapymes/tailwind.config.js
+++ estrapymes/tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {fontFamily: {
+      roboto:['"Roboto Condensed"']
+      },},
+    
   },
   plugins: [],
 }
