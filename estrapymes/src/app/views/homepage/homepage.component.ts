623e1f38eda12d9bae6f153194406e6ae41906b5
Miguel Zapata <miguelzj0909@gmail.com>
2024-07-31 20:53:21 -0500

    Cambios en el homepage

--- estrapymes/src/app/views/homepage/homepage.component.ts
+++ estrapymes/src/app/views/homepage/homepage.component.ts
@@ -13,6 +13,7 @@ export class HomepageComponent implements OnInit {
 
   ngOnInit(): void {
     this.initScrollAnimation();
+    this.applyAnimationOnLoad();
   }
 
   // Navegar a la vista de login
@@ -45,5 +46,44 @@ export class HomepageComponent implements OnInit {
     targets.forEach(target => {
       observer.observe(target);
     });
+  }
+
+  // Aplicar animación a los elementos visibles al cargar la página
+  applyAnimationOnLoad(): void {
+    const targets = document.querySelectorAll('.scroll-trigger');
+    requestAnimationFrame(() => {
+      targets.forEach(target => {
+        if (this.isElementInViewport(target)) {
+          target.classList.add('visible');
+        } else {
+          target.classList.add('initial-visibility');
+        }
+      });
+    });
+  }
+
+  // Verificar si un elemento está en el viewport
+  isElementInViewport(element: Element): boolean {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  // Método para desplazarse a una sección específica y activar la animación
+  scrollToSection(event: Event, sectionId: string): void {
+    event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+      // Añadir clase de animación
+      section.classList.add('animate');
+      // Eliminar la clase después de la animación para permitir futuros clics
+      setTimeout(() => section.classList.remove('animate'), 1000); // 1000 ms debe coincidir con la duración de la animación en CSS
+    }
   }
 }
